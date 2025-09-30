cb1c0039289c13ae04c3de71f3a8348eece7f147
chithong17 <chithongg17@gmail.com>
2025-09-30 13:42:55 +0700

    add file yaml

--- backend/fapanese/src/main/java/com/ktnl/fapanese/controller/UserController.java
+++ backend/fapanese/src/main/java/com/ktnl/fapanese/controller/UserController.java
@@ -1,5 +1,17 @@
 package com.ktnl.fapanese.controller;
 
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
 public class UserController {
 
 }
---
+++ backend/fapanese/src/main/java/com/ktnl/fapanese/entity/User.java
@@ -0,0 +1,27 @@
+package com.ktnl.fapanese.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String id;
+    String username;
+    String email;
+    String password_hash;
+    LocalDateTime created_at;
+    LocalDateTime updated_at;
+}

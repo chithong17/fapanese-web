edd6d595b95f353f91319d49999c9c552398e54e
hqlkhanh <hlqkhanh@gmail.com>
2025-10-01 12:16:10 +0700

    config security p1

--- backend/fapanese/src/main/java/com/ktnl/fapanese/configuration/SecurityConfig.java
+++ backend/fapanese/src/main/java/com/ktnl/fapanese/configuration/SecurityConfig.java
@@ -1,13 +1,24 @@
 package com.ktnl.fapanese.configuration;
 
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.io.IOException;
 
 @Configuration // Đây là class cấu hình cho Spring
 @EnableWebSecurity // Bật tính năng bảo mật web của Spring Security
@@ -26,6 +37,35 @@ public class SecurityConfig {
                         .anyRequest()
                         .authenticated());
 
+//        httpSecurity.oauth2ResourceServer(oauth2 ->
+//                oauth2.jwt(jwtConfigurer ->
+//                        jwtConfigurer.decoder(customJwtDecoder)
+//                                .jwtAuthenticationConverter(jwtAuthenticationConverter()))
+//                        .authenticationEntryPoint(new Auth))
 
+        // Tắt CSRF (thường chỉ bật cho ứng dụng web dùng session; REST API thì tắt đi)
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+
+        return httpSecurity.build();
+    }
+
+
+    /**
+     * Cách đọc thông tin quyền (role/permission) từ trong JWT
+     */
+    @Bean
+    JwtAuthenticationConverter jwtAuthenticationConverter(){
+        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+        // Mặc định Spring sẽ gắn prefix "SCOPE_" vào role
+        // Ví dụ "ADMIN" -> "SCOPE_ADMIN"
+        // Nhưng do token của mình đã lưu sẵn "ROLE_ADMIN" rồi, nên bỏ prefix đi
+        jwtGrantedAuthoritiesConverter.setAuthorityPrefix("");
+
+        // Trả về converter sau khi đã custom
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+
+        return jwtAuthenticationConverter;
     }
 }

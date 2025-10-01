10d5bd9ff635b265bd2919fe470455e454c4d918
chithong17 <chithongg17@gmail.com>
2025-10-01 13:45:32 +0700

    add web config file

---
+++ backend/fapanese/src/main/java/com/ktnl/fapanese/configuration/WebConfig.java
@@ -0,0 +1,21 @@
+package com.ktnl.fapanese.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://192.168.1.20:3000") // máy frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
+}

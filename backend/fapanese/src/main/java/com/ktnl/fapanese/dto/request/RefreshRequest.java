4291fa2fceea64b3076743b9654d992311245e63
hqlkhanh <hlqkhanh@gmail.com>
2025-10-01 21:05:33 +0700

    complete refresh token

---
+++ backend/fapanese/src/main/java/com/ktnl/fapanese/dto/request/RefreshRequest.java
@@ -0,0 +1,14 @@
+package com.ktnl.fapanese.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class RefreshRequest {
+    String token;
+}
+

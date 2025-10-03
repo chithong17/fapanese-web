4d3c06fc37be3af4f7671bbcf79b8105bd1905f1
hqlkhanh <hlqkhanh@gmail.com>
2025-10-03 21:42:42 +0700

    fix firstname lastname

--- backend/fapanese/src/main/java/com/ktnl/fapanese/dto/response/UserResponse.java
+++ backend/fapanese/src/main/java/com/ktnl/fapanese/dto/response/UserResponse.java
@@ -13,17 +13,13 @@ import java.time.LocalDate;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserResponse {
     private String id;
-    private String firstName;
-    private String lastName;
     private String email;
     private String role;
     private String firstname;
     private String lastname;
     private LocalDate dob;
-
     // student specific
     private String campus;
-
     // lecturer specific
     private String expertise;
     private String bio;
--- backend/fapanese/src/main/java/com/ktnl/fapanese/mappper/UserMapper.java
+++ backend/fapanese/src/main/java/com/ktnl/fapanese/mappper/UserMapper.java
@@ -13,16 +13,16 @@ public interface UserMapper {
     @Mapping(target = "password_hash", source = "password")
     User toUser(UserRequest request);
 
-    @Mapping(source = "firstName", target = "firstName")
-    @Mapping(source = "lastName", target = "lastName")
+    @Mapping(source = "firstname", target = "firstname")
+    @Mapping(source = "lastname", target = "lastname")
     @Mapping(
             target = "dateOfBirth",
             expression = "java(request.getDateOfBirth() == null ? null : java.time.LocalDate.parse(request.getDateOfBirth(), java.time.format.DateTimeFormatter.ofPattern(\"yyyy-MM-dd\")))"
     )
     Student toStudent(UserRequest request);
 
-    @Mapping(source = "firstName", target = "firstName")
-    @Mapping(source = "lastName", target = "lastName")
+    @Mapping(source = "firstname", target = "firstname")
+    @Mapping(source = "lastname", target = "lastname")
     @Mapping(
             target = "dateOfBirth",
             expression = "java(request.getDateOfBirth() == null ? null : java.time.LocalDate.parse(request.getDateOfBirth(), java.time.format.DateTimeFormatter.ofPattern(\"yyyy-MM-dd\")))"
@@ -31,11 +31,11 @@ public interface UserMapper {
 
 
     @Mapping(
-            expression = "java(user.getStudent() != null ? user.getStudent().getFirstName() : (user.getTeacher() != null ? user.getTeacher().getFirstName() : null))",
+            expression = "java(user.getStudent() != null ? user.getStudent().getfirstName() : (user.getTeacher() != null ? user.getTeacher().getFirstname() : null))",
             target = "firstname"
     )
     @Mapping(
-            expression = "java(user.getStudent() != null ? user.getStudent().getLastName() : (user.getTeacher() != null ? user.getTeacher().getLastName() : null))",
+            expression = "java(user.getStudent() != null ? user.getStudent().getlastName() : (user.getTeacher() != null ? user.getTeacher().getLastname() : null))",
             target = "lastname"
     )
     @Mapping(

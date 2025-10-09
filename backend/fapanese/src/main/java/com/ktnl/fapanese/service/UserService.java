dd2ff764105b98559d6b6b3405ae6c45cceaf2e6
hqlkhanh <hlqkhanh@gmail.com>
2025-10-09 11:21:53 +0700

    fix dob front-end and back-end

--- backend/fapanese/src/main/java/com/ktnl/fapanese/service/UserService.java
+++ backend/fapanese/src/main/java/com/ktnl/fapanese/service/UserService.java
@@ -101,7 +101,7 @@ public class UserService {
 
         // Nếu là student
         if (user.getStudent() != null) {
-            builder.dob(user.getStudent().getDateOfBirth())
+            builder.dateOfBirth(user.getStudent().getDateOfBirth())
                     .campus(user.getStudent().getCampus())
                     .firstName(user.getStudent().getFirstName())
                     .lastName(user.getStudent().getLastName());
@@ -109,7 +109,7 @@ public class UserService {
 
         // Nếu là lecturer
         if (user.getTeacher() != null) {
-            builder.dob(user.getTeacher().getDateOfBirth())
+            builder.dateOfBirth(user.getTeacher().getDateOfBirth())
                     .expertise(user.getTeacher().getExpertise())
                     .bio(user.getTeacher().getBio())
                     .firstName(user.getTeacher().getFirstName())

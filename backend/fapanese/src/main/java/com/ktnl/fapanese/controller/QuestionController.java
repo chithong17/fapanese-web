e0270c77531be444a325c8f7fb9dcd05725f98ee
chithong17 <chithongg17@gmail.com>
2025-10-17 22:35:00 +0700

    update entity

--- backend/fapanese/src/main/java/com/ktnl/fapanese/controller/QuestionController.java
+++ backend/fapanese/src/main/java/com/ktnl/fapanese/controller/QuestionController.java
@@ -1,7 +1,10 @@
 package com.ktnl.fapanese.controller;
 
 import com.ktnl.fapanese.dto.request.QuestionRequest;
+import com.ktnl.fapanese.dto.request.UserAnswer;
+import com.ktnl.fapanese.dto.response.ApiResponse;
 import com.ktnl.fapanese.dto.response.QuestionResponse;
+import com.ktnl.fapanese.dto.response.SubmitQuizResponse;
 import com.ktnl.fapanese.service.interfaces.IQuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -51,5 +54,19 @@ public class QuestionController {
     @GetMapping("/category/{category}")
     public ResponseEntity<List<QuestionResponse>> getQuestionsByCategory(@PathVariable String category) {
         return ResponseEntity.ok(questionService.getQuestionsByCategory(category));
+    }
+
+
+    @PostMapping("/submit")
+    public ApiResponse<SubmitQuizResponse> submitAnswers(@RequestBody List<UserAnswer> userAnswers) {
+        SubmitQuizResponse result = questionService.checkAndSubmitAnswers(userAnswers);
+
+        String message = String.format("Quiz submitted successfully. Score: %.2f%% (%d/%d)",
+                result.getScorePercentage(), result.getCorrectCount(), result.getTotalQuestions());
+
+        return ApiResponse.<SubmitQuizResponse>builder()
+                .message(message)
+                .result(result)
+                .build();
     }
 }

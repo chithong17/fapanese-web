fb84109ec9980181ef0f0b2ef57bb77bcbc41e40
hqlkhanh <hlqkhanh@gmail.com>
2025-10-21 08:34:49 +0700

    config Swagger

--- backend/fapanese/src/main/java/com/ktnl/fapanese/service/interfaces/IStudentService.java
+++ backend/fapanese/src/main/java/com/ktnl/fapanese/service/interfaces/IStudentService.java
@@ -3,23 +3,19 @@ package com.ktnl.fapanese.service.interfaces;
 import com.ktnl.fapanese.dto.request.StudentRegisterResquest;
 import com.ktnl.fapanese.dto.response.StudentRegisterResponse;
 import com.ktnl.fapanese.dto.response.UserResponse;
-import org.springframework.security.access.prepost.PreAuthorize;
+// import org.springframework.security.access.prepost.PreAuthorize; // Có thể xóa import này
 
 import java.util.List;
 
 public interface IStudentService {
-    @PreAuthorize("hasAuthority('REGISTER_STUDENT')")
+
     StudentRegisterResponse registerStudent(StudentRegisterResquest student);
 
-    @PreAuthorize("hasAnyRole('ADMIN', 'LECTURER')")
     List<UserResponse> getAllStudent();
 
-    @PreAuthorize("hasAnyRole('ADMIN', 'LECTURER')")
     UserResponse getStudentByEmail(String email);
 
-    @PreAuthorize("hasAnyRole('ADMIN', 'LECTURER')")
     UserResponse updateStudent(String email, StudentRegisterResquest studentUpdateRequest);
 
-    @PreAuthorize("hasRole('ADMIN')")
     void deleteStudent(String email);
 }

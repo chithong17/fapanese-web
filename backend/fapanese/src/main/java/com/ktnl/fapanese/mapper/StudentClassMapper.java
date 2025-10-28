d38acfbfdb61b1c584199d432b17ada320cadfc5
hqlkhanh <hlqkhanh@gmail.com>
2025-10-29 00:20:04 +0700

    write api for Lecturer manage Class

--- backend/fapanese/src/main/java/com/ktnl/fapanese/mapper/StudentClassMapper.java
+++ backend/fapanese/src/main/java/com/ktnl/fapanese/mapper/StudentClassMapper.java
@@ -1,4 +1,16 @@
 package com.ktnl.fapanese.mapper;
 
-public class StudentClassMapper {
+
+import com.ktnl.fapanese.dto.response.StudentClassResponse;
+import com.ktnl.fapanese.entity.StudentClass;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import javax.xml.transform.Source;
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = {UserMapper.class, ClassCourseMapper.class})
+public interface StudentClassMapper {
+    StudentClassResponse toStudentClassResponse(StudentClass studentClass);
+    List<StudentClassResponse> toStudentClassResponses(List<StudentClass> list);
 }

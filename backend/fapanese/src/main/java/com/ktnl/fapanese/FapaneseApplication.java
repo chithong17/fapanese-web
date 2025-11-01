d9f60d871ba3cd082f298b5d74a408e7b7568f31
longfptaka3011 <trannguyenbaolong3011@gmail.com>
2025-11-01 22:21:45 +0700

    fix

--- backend/fapanese/src/main/java/com/ktnl/fapanese/FapaneseApplication.java
+++ backend/fapanese/src/main/java/com/ktnl/fapanese/FapaneseApplication.java
@@ -1,13 +1,13 @@
-package com.ktnl.fapanese;
+    package com.ktnl.fapanese;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+    import org.springframework.boot.SpringApplication;
+    import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class FapaneseApplication {
+    @SpringBootApplication
+    public class FapaneseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FapaneseApplication.class, args);
-	}
+        public static void main(String[] args) {
+            SpringApplication.run(FapaneseApplication.class, args);
+        }
 
-}
+    }

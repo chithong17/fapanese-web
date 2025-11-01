cea0237189d3b2e809475166d07c7dc1e551f3bb
hqlkhanh <hlqkhanh@gmail.com>
2025-11-01 22:14:42 +0700

    impl api AI speaking-test

--- backend/fapanese/src/main/java/com/ktnl/fapanese/dto/response/SpeakingTestQuestionResponse.java
+++ backend/fapanese/src/main/java/com/ktnl/fapanese/dto/response/SpeakingTestQuestionResponse.java
@@ -1,4 +1,21 @@
 package com.ktnl.fapanese.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class SpeakingTestQuestionResponse {
+    Long id;
+    String question;
+    String questionRomaji;
+    String questionMeaning;
+    String answer;
+    String answerRomaji;
+    String answerMeaning;
 }
